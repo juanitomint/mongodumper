45e609c72319daca2b48e511d629741c0d3970c9
Juan Ignacio Borda <juanignacioborda@gmail.com>
2019-03-18 12:52:44 -0300

    updated microbadger hook

--- push.sh
+++ push.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
  docker push juanitomint/mongodumper
 # notify microbadger
-curl -X POST https://hooks.microbadger.com/images/juanitomint/nextcloud-client/_oZQ1x16Y_503mZGoIVtirP7sEs=
+curl -X POST https://hooks.microbadger.com/images/juanitomint/mongodumper/0SeTwOxenAqX-AYnkbhsvfI0dqo=
